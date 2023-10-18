e47f0f446d8c609a379bb1fe2ed658049267c716
samjaygill <138401809+samjaygill@users.noreply.github.com>
2023-10-18 18:38:24 +0100

    download cv

--- src/components/CVInfo.js
+++ src/components/CVInfo.js
@@ -2,10 +2,13 @@ import cv from './images/SG_CV_2023.pdf'
 import './css/CVInfo.css'
 
 const CVInfo = () => {
-    return ( 
+    return (
+        <div className="cvInfo">
+        <p><a href={cv} download="Samantha_Gillies.pdf" className="downloadCV">Click here to download</a></p>
         <div className="cvPDF">
         <iframe id="myPDF" src={cv} frameborder="0" title='cv'>
         </iframe>
+        </div>
         </div>
      );
 }
