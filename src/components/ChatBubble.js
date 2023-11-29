c46a64fb931e5743f22aee704726ebf55c717a23
samjaygill <138401809+samjaygill@users.noreply.github.com>
2023-11-29 16:18:27 +0000

    updates to imessage on home

--- src/components/ChatBubble.js
+++ src/components/ChatBubble.js
@@ -6,19 +6,19 @@ const ChatBubble = () => {
     <div class="container">
       <div class="imessage">
         <p class="from-them">
-          Hello World! I'm <b>Samantha</b>, Welcome to my portfolio!
+          Hello World! I'm Samantha, Welcome to my portfolio!
         </p>
         <p class="from-me">Hi Samantha. Where would i find your projects?</p>
         <p class="from-them ">
-          You would find my projects <Link to={`/projects/`}>here</Link>!
+          You would find my projects <Link to={`/projects/`} className="cb-link">here</Link>!
         </p>
         <p class="from-me">How would I find out more about you?</p>
         <p class="from-them ">
-          You can find out more info about me <Link to={`/about/`}>here</Link>!
+          You can find out more about me <Link to={`/about/`} className="cb-link">here</Link>!
         </p>
         <p class="from-them margin-b_one">
           If you would like to get in touch feel free to{" "}
-          <a href="mailto:samantha.gillies@outlook.com?subject=Portfolio">
+          <a href="mailto:samantha.gillies@outlook.com?subject=Portfolio" className="cb-link">
             email me
           </a>
           .
