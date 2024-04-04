bbcdf6af5b9ff90a8f99b5817f1566aa85d51968
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-04-04 14:59:20 +0100

    changes to styling and cv

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import "./css/home.css";
 import styled from "styled-components";
 import HomeImage from "./HomeImage";
 import ChatBubble from "./ChatBubble";
-import ProgressBar from "@ramonak/react-progress-bar";
 
 const HomeHeading = styled.h3`
   text-align: center;
@@ -46,61 +45,6 @@ const Home = ({skills}) => {
           <div key={index} className="skills">{skill.toUpperCase()}</div>
         ))}
       </div>
-{/* 
-      <div className="container-2">
-        <div className="progress-1">
-          <p>PYTHON</p>
-          <hr className="line" />
-          <p>Skill level:</p>
-
-          <ProgressBar
-            completed={85}
-            className="wrapper"
-            barContainerClassName="barContainer"
-            completedClassName="barCompleted1"
-            labelClassName="label"
-          />
-        </div>
-        <div className="progress-2">
-          <p>JAVASCRIPT</p>
-          <hr className="line" />
-          <p>Skill level:</p>
-
-          <ProgressBar
-            completed={90}
-            className="wrapper"
-            barContainerClassName="barContainer"
-            completedClassName="barCompleted2"
-            labelClassName="label"
-          />
-        </div>
-        <div className="progress-3">
-          <p>JAVA</p>
-          <hr className="line" />
-          <p>Skill level:</p>
-
-          <ProgressBar
-            completed={85}
-            className="wrapper"
-            barContainerClassName="barContainer"
-            completedClassName="barCompleted3"
-            labelClassName="label"
-          />
-        </div>
-        <div className="progress-4">
-          <p>TYPESCRIPT (Learning)</p>
-          <hr className="line" />
-          <p>Skill level:</p>
-
-          <ProgressBar
-            completed={5}
-            className="wrapper"
-            barContainerClassName="barContainer"
-            completedClassName="barCompleted4"
-            labelClassName="label"
-          />
-        </div> */}
-      {/* </div> */}
     </>
   );
 };
